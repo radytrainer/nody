75c94a32111ef35c06c8c604f0387bb621459373
rady y <rady.y@passerellesnumeriques.org>
2021-06-23 15:32:32 +0700

    Update database.js

--- database.js
+++ database.js
@@ -4,8 +4,8 @@ const {createPool} = require('mysql');
 
 const db = createPool({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'superuser',
+    password: '0o9EtUSJejb2IYdg ',
     database: 'nody_db',
     connectionLimit: 10
 });
